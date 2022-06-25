ebdbe7a1a17efe8421b9f49e36bd516d5fda7279
JunHwan Kim <nefertiri972@gmail.com>
2022-06-26 00:11:34 +0900

    Add JPA Repository and Configuration
    - Item 클래스 ORM 매핑
    - JPA 기반 리포지토리 구현
    - JPA Configuration 등록

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,23 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+//@Table(name = "item")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA는 public 또는 protected 기본 생성자가 반드시 있어야 함
     public Item() {
     }
 
